5f95e22104ebe56eddd40a348097639545b453a7
Jun Gu <druidream@gmail.com>
2024-04-28 15:38:42 +0800

    Chapter 2. Variables, Constants, and Types

---
+++ Sandbox/src/TypeIntro.kt
@@ -0,0 +1,12 @@
+const val MAX_EXPERIENCE: Int = 5000
+
+fun main(args: Array<String>) {
+    val playerName = "Estragon"
+    var experiencePoints = 5
+    experiencePoints += 5
+    println(experiencePoints)
+    println(playerName)
+}
+
+// Control+Shift+P
+// Shift+Shift
