7495812558413d5d7829a7d825e9d166c13ce220
Jun Gu <druidream@gmail.com>
2024-05-04 19:16:37 +0800

    Chapter 4. Functions

--- NyetHack/src/Game.kt
+++ NyetHack/src/Game.kt
@@ -1,12 +1,13 @@
 fun main(args: Array<String>) {
-    val name = "Madrigal"
-    var healthPoints = 89
+    val name: String
+    name = "Madrigal"
+    var healthPoints: Int
+    healthPoints = 89
     val isBlessed = true
     val isImmortal = false
 
     // Aura
-    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-    val auraColor = if (auraVisible) "GREEN" else "NONE"
+    val auraColor = auraColor(isBlessed, healthPoints, isImmortal)
     println(auraColor)
 
     /*
@@ -26,21 +27,10 @@ fun main(args: Array<String>) {
         "is in awful condition!"
     }
     */
-    val healthStatus = when (healthPoints) {
-        100 -> "is in excellent condition!"
-        in 90..99 -> "has a few scratches."
-        in 75..89 -> if (isBlessed) {
-            "has some minor wounds but is healing quite quickly!"
-        } else {
-            "has some minor wounds."
-        }
-        in 15..74 -> "looks pretty hurt."
-        else -> "is in awful condition!"
-    }
+    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
 
     // Player status
-    println("(Aura: $auraColor) " + "(Blessed: ${if (isBlessed) "YES" else "NO"})")
-    println("$name $healthStatus")
+    printPlayerStatus(auraColor, isBlessed, name, healthStatus)
 
     1 in 1..3
     (1..3).toList()
@@ -50,6 +40,74 @@ fun main(args: Array<String>) {
     2 in 1..3
     2 !in 1..3
     'x' in 'a'..'z'
+
+    performCombat()
+    performCombat("Ulrich")
+    performCombat("Hildr", true)
 }
 
-// Control+Option+I
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) =
+    when (healthPoints) {
+        100 -> "is in excellent condition!"
+        in 90..99 -> "has a few scratches."
+        in 75..89 -> if (isBlessed) {
+            "has some minor wounds, but is healing quite quickly!"
+        } else {
+            "has some minor wounds."
+        }
+
+        in 15..74 -> "looks pretty hurt."
+        else -> "is in awful condition!"
+    }
+
+private fun printPlayerStatus(
+    auraColor: String,
+    isBlessed: Boolean,
+    name: String,
+    healthStatus: String
+) {
+    println("(Aura: $auraColor) " + "(Blessed: ${if (isBlessed) "YES" else "NO"})")
+    println("$name $healthStatus")
+    castFireball()
+}
+
+private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean): String {
+    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+    val auraColor = if (auraVisible) "GREEN" else "NONE"
+    return auraColor
+}
+
+private fun castFireball(numFireballs: Int = 2) =
+    println("A glass of Fireball springs into existence. (x$numFireballs)")
+
+fun shouldReturnAString(): String {
+    TODO()
+    println("unreachable")
+}
+
+fun performCombat() {
+    println("You see nothing to fight!")
+}
+
+fun performCombat(enemyName: String) {
+    println("You begin fighting $enemyName.")
+}
+
+fun performCombat(enemyName: String, isBlessed: Boolean) {
+    if (isBlessed) {
+        println("You begin fighting $enemyName. You are blessed with 2X damage!")
+    } else {
+        println("You begin fighting $enemyName.")
+    }
+}
+
+// Control-Option-I: format
+// Control-Shift-P: show return type
+// Shift-Shift: search everywhere
+
+
+// tipsy
+// sloshed
+// soused
+// stewed
+// t0aSt3d
